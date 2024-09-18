8ef9778ebdc1a877532027667cf8f23eba45f395
yurifws <yurifws@gmail.com>
2024-09-18 12:08:12 -0300

    feat: mapeamento de entidades e modelos

--- src/main/java/br/com/app/product/model/ProductResponseModel.java
+++ src/main/java/br/com/app/product/model/ProductResponseModel.java
@@ -1,5 +1,21 @@
 package br.com.app.product.model;
 
+import java.time.LocalDateTime;
+
+import br.com.app.product.enuns.ProductStatusEnum;
+import lombok.Data;
+
+@Data
 public class ProductResponseModel {
+	
+	private Long id;
+	
+	private String description;
+	
+	private ProductStatusEnum status;
+	
+	private LocalDateTime dateCreated;
+	
+	private LocalDateTime dateUpdated;
 
 }
